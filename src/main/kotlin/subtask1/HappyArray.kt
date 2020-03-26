5c3490e0024dd14803e2c02e04ba2bfcedae898f
softoed <4elovek-fakel@mail.ru>
2020-03-26 13:54:45 +0300

    Add: 3 of 5 tasks

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,21 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var tempArray = sadArray.toMutableList()
+        if (sadArray.count() < 3) return sadArray
+
+        var i: Int = 0
+
+        do if (tempArray.elementAt(i) + tempArray.elementAt(i+2) < tempArray.elementAt(i+1)) {
+            tempArray.removeAt(i+1)
+            if (i != 0) i -= 1
+        }
+        else
+        {
+            if (i != tempArray.count() - 2) i += 1
+        }
+        while (i != tempArray.count() - 2)
+
+        return tempArray.toIntArray()
     }
 }
--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -4,6 +4,17 @@ class MiniMaxSum {
 
     // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var numberForRes = input.min()
+        var minNumber = 0
+        if (numberForRes != null) minNumber = numberForRes
+
+        numberForRes = input.max()
+        var maxNumber = 0
+        if (numberForRes != null) maxNumber = numberForRes
+
+        val maxSum = input.sum() - minNumber
+        val minSum = input.sum() - maxNumber
+
+        return intArrayOf(minSum, maxSum)
     }
 }
